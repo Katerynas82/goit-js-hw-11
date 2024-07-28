8304b399d880dcc7e93c6b316fbb9883308c234a
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-07-28 16:28:35 +0100

    final

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,10 +3,10 @@
 export async function searchGalleryQuery(query) {
     const URL = "https://pixabay.com/api/";
     const API_KEY = "38315380-10eeb0cf3b6e9a9ad49e9c507";
-    const perPage = 21;
+    
 
 
-    return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}`)
+    return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then((response) => {
             if (!response.ok) {
                 throw new Error(response.status);
